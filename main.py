bceca795c8634818051db28094740c84c8b37e85
rafaelpbeckmann <rafaelpbeckmann@gmail.com>
2026-06-18 11:11:20 -0300

    etapa 2 do projeto

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import os
-Print("hello world");
+caminhos = r"C:\Users\rafae\Downloads"
+pastas_objetivo = {
+    ".pdf": "arquivos_pdf",
+    ".docx": "arquivos_docx",
+    ".xlsx": "planilhas",
+    ".jpg": "imagens",
+    ".png": "imagens",
+    ".zip": "arquivos_comprimidos",
+    ".rar": "arquivos_comprimidos",
+    ".iso": "arquivos_comprimidos",
+    ".exe": "executaveis",
+    ".pptx": "apresentacoes",
+    ".jpeg": "imagens",
+    ".gif": "imagens",
+    ".ppt": "apresentacoes",
+}
+arquivos = os.listdir(caminhos)
+for arquivo in arquivos:
+    nome_arquivo, extensao = os.path.splitext(arquivo)
+    pasta_destino = pastas_objetivo.get(extensao, "outros")
+    caminho_completo_pasta = os.path.join(caminhos, pasta_destino)
+    print(f"Arquivo: {arquivo} | Destino: {caminho_completo_pasta}")
